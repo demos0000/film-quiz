8b0a529545f93ce084bb7b93b63970cb5ba60fbc
luis <nospam@gmail.com>
2021-07-06 07:11:41 -0700

    updating demo url

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Film trivia quiz Demo
 
-To view the live demo click [here](http://quiz.luishernandez.xyz)
+To view the live demo click [here](https://dv.d3oyin4fe68kzp.amplifyapp.com/)
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Amplify from "aws-amplify";
-// import awsExports from "./aws-exports";
-// Amplify.configure(awsExports);
-
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
